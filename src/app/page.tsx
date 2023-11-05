8997a487911c2d05a19b84e11319c0f7a64c2c31
Brandon Rubio <brubio3724@gmail.com>
2023-11-05 18:04:27 -0500

    created first mvp for tipping calculator

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,70 @@
 'use client';
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export default function Home() {
-  const [search, setSearch] = useState('');
+  const [bill, setBill] = useState('');
+  const [tipPercentage, setTipPercentage] = useState(5);
+  const [tipAmount, setTipAmount] = useState('0.00');
+  const [total, setTotal] = useState('0.00');
+
+  useEffect(() => {
+    if (bill !== '') {
+      setTipAmount(calculateTip(bill, tipPercentage).toFixed(2));
+      setTotal((Number(bill) + Number(tipAmount)).toFixed(2));
+    }
+  }, [bill, tipAmount, tipPercentage]);
+
+  function isNumberAValidDollarAmount(number: string): boolean {
+    const dollarPattern = /^\d+(\.\d{0,2})?$/;
+
+    if (!dollarPattern.test(number) && number !== '') {
+      return false;
+    }
+
+    return true;
+  }
+
+  function calculateTip(bill: string, tip: number): number {
+    if (bill === '') {
+      return 0;
+    }
+
+    const billNumber = Number(bill);
+    const tipNumber = billNumber * (tip / 100);
+
+    return tipNumber;
+  }
 
   return (
-    <main className="flex min-h-screen flex-col items-center p-24">
-      <form
-        onSubmit={async (e) => {
-          e.preventDefault();
-          setSearch('');
-        }}
-        className="flex gap-2"
-      >
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <div className="flex flex-col border rounded-sm p-3 gap-2">
+        <label className="text-white">Enter bill</label>
         <input
           type="text"
-          value={search}
-          placeholder="Anime girl with blue hair..."
-          onChange={(e) => setSearch(e.target.value)}
-          className="bg-neutral-800 border border-neutral-800 rounded-sm px-5 py-2 outline-none text-sm"
+          value={bill}
+          placeholder="0.00"
+          onChange={(e) => {
+            if (isNumberAValidDollarAmount(e.target.value)) {
+              setBill(e.target.value);
+            }
+          }}
+          className="bg-black border"
         />
-        <button type="submit" className="bg-white text-black rounded-sm p-2 text-sm">
-          Generate
-        </button>
-      </form>
+        <label className="text-white">{`Tip Percentage: ${tipPercentage}%`}</label>
+        <input
+          type="range"
+          min={5}
+          max={25}
+          value={tipPercentage}
+          onChange={(e) => {
+            setTipPercentage(Number(e.target.value));
+          }}
+        />
+        <span>{`Tip Amount: ${tipAmount}`}</span>
+        <span>{`Total: ${total}`}</span>
+      </div>
+      <div></div>
     </main>
   );
 }

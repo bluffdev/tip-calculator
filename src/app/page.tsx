2bee019bcfc009e000cf4d19bbd1fb02d964fde3
Brandon Rubio <brubio3724@gmail.com>
2023-11-07 10:57:44 -0500

    updated input ui and labels for accessability

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ export default function Home() {
   const [tipPercentage, setTipPercentage] = useState(5);
   const [tipAmount, setTipAmount] = useState('0.00');
   const [total, setTotal] = useState('0.00');
+  const [otherSelected, setOtherSelected] = useState(false);
 
   useEffect(() => {
     if (bill !== '') {
@@ -38,9 +39,12 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <div className="flex flex-col border rounded-sm p-3 gap-2">
-        <label className="text-white">Enter bill</label>
+      <div className="flex flex-col p-6 gap-4 lg:border lg:rounded-md">
+        <label htmlFor="bill-input" className="text-white text-lg">
+          Enter Bill
+        </label>
         <input
+          id="bill-input"
           type="text"
           value={bill}
           placeholder="0.00"
@@ -49,33 +53,63 @@ export default function Home() {
               setBill(e.target.value);
             }
           }}
-          className="bg-black border"
+          className="bg-neutral-900 focus:outline-none focus:ring-offset-1 focus:ring focus:ring-offset-neutral-300 focus:ring-neutral-500 rounded-md text-md p-2"
         />
-        <label className="text-white">{`Tip Percentage: ${tipPercentage}%`}</label>
-        <div className="flex gap-2">
-          <button onClick={() => setTipPercentage(15)} className="border rounded-sm px-2 py-1">
+        <label htmlFor="tipPercentage" className="flex justify-between text-white text-lg">
+          <span>Tip Percentage:</span>
+          <span id="tipPercentage" className="ml-2">{`${tipPercentage}%`}</span>
+        </label>
+
+        <div className="flex gap-2 text-black">
+          <button
+            onClick={() => setTipPercentage(15)}
+            className="bg-white border rounded-md px-3 py-1"
+          >
             15%
           </button>
-          <button onClick={() => setTipPercentage(18)} className="border rounded-sm px-2 py-1">
+          <button
+            onClick={() => setTipPercentage(18)}
+            className="bg-white border rounded-md px-3 py-1"
+          >
             18%
           </button>
-          <button onClick={() => setTipPercentage(20)} className="border rounded-sm px-2 py-1">
+          <button
+            onClick={() => setTipPercentage(20)}
+            className="bg-white border rounded-md px-3 py-1"
+          >
             20%
           </button>
+          <button
+            aria-label="Enter other percentage"
+            onClick={() => setOtherSelected(!otherSelected)}
+            className="bg-white border rounded-md px-3 py-1"
+          >
+            Other
+          </button>
         </div>
-        <input
-          type="range"
-          min={5}
-          max={25}
-          value={tipPercentage}
-          onChange={(e) => {
-            setTipPercentage(Number(e.target.value));
-          }}
-        />
-        <span>{`Tip Amount: ${tipAmount}`}</span>
-        <span>{`Total: ${total}`}</span>
+        {otherSelected && (
+          <input
+            type="text"
+            value={bill}
+            placeholder="0"
+            onChange={(e) => {
+              if (isNumberAValidDollarAmount(e.target.value)) {
+                setBill(e.target.value);
+              }
+            }}
+            className="bg-neutral-900 focus:outline-none focus:ring-offset-1 focus:ring focus:ring-offset-neutral-300 focus:ring-neutral-500 rounded-md text-md p-2"
+          />
+        )}
+        <hr />
+        <div className="flex justify-between">
+          <span className="text-lg">Tip Amount:</span>
+          <span className="text-lg">{`$${tipAmount}`}</span>
+        </div>
+        <div className="flex justify-between">
+          <span className="text-lg">Total:</span>
+          <span className="text-lg">{`$${total}`}</span>
+        </div>
       </div>
-      <div></div>
     </main>
   );
 }
